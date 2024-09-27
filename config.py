29b2c83ef4b3fbd1d8b20ef87c2dc63ab7d3daff
Вадим <kantora_70@mail.ru>
2024-09-27 08:35:32 +0500

    Initial commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Токен бота, который получен от BotFather
-TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+TOKEN = 'TELEGRAM_TOKEN'
 
 # API для получения курса валют
 API_URL = 'https://open.er-api.com/v6/latest/USD'  # Пример с Open Exchange Rates API
